bc1bdd5c1d01fa519ad0d0a6bda06e5b1ae88557
RezaDeveloper97 <RezaDeveloper97@gmail.com>
2025-02-08 17:43:22 +0330

    add EventSourcingTrait in all model

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
+use App\Traits\EventSourcingTrait;
 use Illuminate\Database\Eloquent\Model;
 
 abstract class BaseModel extends Model
 {
-
+    use EventSourcingTrait;
 }
