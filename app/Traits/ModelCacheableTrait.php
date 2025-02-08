e7525460e5ca8d34c46917579bda9e77f1969191
RezaDeveloper97 <RezaDeveloper97@gmail.com>
2025-02-08 19:40:16 +0330

    refactor ModelCacheableTrait

--- app/Traits/ModelCacheableTrait.php
+++ app/Traits/ModelCacheableTrait.php
@@ -2,40 +2,31 @@
 
 namespace App\Traits;
 
-use Illuminate\Support\Facades\Cache;
+use App\Helpers\CacheHelper;
 use Illuminate\Support\Str;
 
 trait ModelCacheableTrait
 {
-    public function cacheDataWithTags($key, $data, array $tags, $ttl = 3600): void
-    {
-        Cache::tags($tags)->put($key, $data, $ttl);
-    }
-
-    public function getFromCache($key, array $tags)
-    {
-        return Cache::tags($tags)->get($key);
-    }
-
-    public function clearCacheByTags(array $tags): void
-    {
-        Cache::tags($tags)->flush();
-    }
-
-    public function cacheData(): void
+    public function getCacheData()
     {
         $key = $this->getCacheKey();
         $tags = $this->getCacheTags();
-        $data = $this->toArray();
+        return CacheHelper::getFromCache($key, $tags);
+    }
 
-        $this->cacheDataWithTags($key, $data, $tags);
+    public function setCacheData(): void
+    {
+        $key = $this->getCacheKey();
+        $tags = $this->getCacheTags();
+
+        CacheHelper::cacheDataWithTags($key, $this, $tags);
     }
 
     public function clearCache(): void
     {
         $tags = $this->getCacheTags();
 
-        $this->clearCacheByTags($tags);
+        CacheHelper::clearCacheByTags($tags);
     }
 
     protected function getCacheKey(): string
