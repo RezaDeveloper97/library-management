ddd51c78863e2d0cda291e16414a1e70aa0a7a4d
RezaDeveloper97 <RezaDeveloper97@gmail.com>
2025-02-09 12:09:31 +0330

    refactor api

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,12 @@ use App\Http\Controllers\UserAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('auth')
+    ->controller(UserAuthController::class)
+    ->group(function () {
+        Route::post('register', 'register')->name('register');
+        Route::post('login', 'login')->name('login');
+        Route::post('logout', 'logout')->name('logout')->middleware('auth:sanctum');
+    });
 
-Route::post('register', [UserAuthController::class, 'register']);
-Route::post('login', [UserAuthController::class, 'login']);
-Route::post('logout', [UserAuthController::class, 'logout'])
-    ->middleware('auth:sanctum');
+
